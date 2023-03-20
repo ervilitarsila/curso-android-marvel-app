69980937f760c5121feb43e1592ca4f55a458bd0
ervili.tarsila <ervilitarsila@gmail.com>
2023-03-20 13:02:32 -0300

    Mapeamento dos objetos de response da api da marvel

---
+++ core/src/main/java/com/example/core/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,8 @@
+package com.example.core.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ThumbnailResponse(
+    @SerializedName("path") val path: String,
+    @SerializedName("extesion") val extesion: String
+)
