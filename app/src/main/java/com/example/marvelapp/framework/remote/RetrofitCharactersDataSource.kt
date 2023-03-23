40626c592bee8a29892cc7ea9abc118c503dad22
ervili.tarsila <ervilitarsila@gmail.com>
2023-03-23 12:10:56 -0300

    Implementa repositorio na camada de framework

---
+++ app/src/main/java/com/example/marvelapp/framework/remote/RetrofitCharactersDataSource.kt
@@ -0,0 +1,14 @@
+package com.example.marvelapp.framework.remote
+
+import com.example.core.data.repository.CharactersRemodeDataSource
+import com.example.marvelapp.framework.network.MarvelApi
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import javax.inject.Inject
+
+class RetrofitCharactersDataSource @Inject constructor(
+    private val marvelApi: MarvelApi
+) : CharactersRemodeDataSource<DataWrapperResponse> {
+    override suspend fun fetchCharacters(queries: Map<String, String>): DataWrapperResponse {
+        return marvelApi.getCharacters(queries)
+    }
+}
