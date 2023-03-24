10211ceff18931074cc9fb1e8cd3d3de491fceaf
ervili.tarsila <ervilitarsila@gmail.com>
2023-03-24 15:33:38 -0300

    Implementa CharactersPagingSource

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,9 +1,17 @@
 package com.example.marvelapp.framework.network.response
 
 import com.google.gson.annotations.SerializedName
+import com.example.core.domain.model.Character
 
 data class CharacterResponse(
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
     @SerializedName("thumbnail") val thumbnail: ThumbnailResponse
 )
+
+fun CharacterResponse.toCharacterModel(): Character{
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnail.path}.${this.thumbnail.extesion}"
+    )
+}
